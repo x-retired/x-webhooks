9ff92c862880dc8d84e71a148717478adb2bd92f
xiexianbin <xiexianbin@yovole.com>
2018-07-29 20:59:08 +0800

    update url bug

--- webhooks/webhooks/urls.py
+++ webhooks/webhooks/urls.py
@@ -22,5 +22,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('account.urls')),
-    url(r'social-auth/', include('social_django.urls', namespace='social'))
+    url(r'social-auth/', include('social_django.urls', namespace='social')),
+    url(r'^web/', include('web.urls')),
 ]
